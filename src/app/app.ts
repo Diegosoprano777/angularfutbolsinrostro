1de598adb45cfab40911a9bfe2a625add4c40652
JuanDi-c <solanocorreajuandiego@gmail.com>
2026-02-12 10:39:57 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
+import { FooterComponent } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  standalone: true, 
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
   protected readonly title = signal('futbolito');
 }
